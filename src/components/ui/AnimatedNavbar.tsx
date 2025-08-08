274f80e351216b88de0ed1fdd15cf4b17fa65705
Blue Filipe Pinto <filipeecopin025@gmail.com>
2025-08-08 14:41:53 +0300

    Move animated navbar slightly higher

--- src/components/ui/AnimatedNavbar.tsx
+++ src/components/ui/AnimatedNavbar.tsx
@@ -8,7 +8,7 @@ export function AnimatedNavbar({ className }: { className?: string }) {
   
   return (
     <div
-      className={cn("fixed top-0 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed -top-2 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Travel Tools">
